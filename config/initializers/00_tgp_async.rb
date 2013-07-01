73b5d938682b25cd68327d0e47fb735f58bd144b
mark <mark@thegiantpixel>
2013-07-01 13:32:50 -0700

    configuring remote redis on staging and prod

--- config/initializers/00_tgp_async.rb
+++ config/initializers/00_tgp_async.rb
@@ -4,7 +4,7 @@ if Tgp::Async::Engine.config.tgp_async_on
     ActiveRecord::Base.establish_connection 
   }
 
-  if Rails.env.production?
+  if !Rails.env.development?
     uri = URI.parse(ENV[REDIS_ENV_VAR])
     puts "REDIS URI #{uri.inspect}"
     Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
