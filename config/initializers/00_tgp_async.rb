8fba6331cd9ee8e435e5c1b876860a8a14882276
mark <mark@thegiantpixel>
2013-07-01 12:17:57 -0700

    adding a redis initializer

---
+++ config/initializers/00_tgp_async.rb
@@ -0,0 +1,14 @@
+if Tgp::Async::Engine.config.tgp_async_on
+  Resque.after_fork = Proc.new { 
+    Resque.redis.client.reconnect
+    ActiveRecord::Base.establish_connection 
+  }
+
+  if Rails.env.production?
+    uri = URI.parse(ENV[REDIS_ENV_VAR])
+    puts "REDIS URI #{uri.inspect}"
+    Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
+
+  Rails.logger.info('Connected to Redis')
+end
