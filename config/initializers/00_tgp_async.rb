dc7fed0d2c93e6eaac12c909d7f9c10daeca8116
mark <mark@thegiantpixel>
2013-07-01 13:44:10 -0700

    defining a constant that was undefined

--- config/initializers/00_tgp_async.rb
+++ config/initializers/00_tgp_async.rb
@@ -1,10 +1,12 @@
+REDIS_ENV_VAR = "REDISTOGO_URL"
+
 if Tgp::Async::Engine.config.tgp_async_on
   Resque.after_fork = Proc.new { 
     Resque.redis.client.reconnect
     ActiveRecord::Base.establish_connection 
   }
 
-  if !Rails.env.development?
+  if ENV[REDIS_ENV_VAR]
     uri = URI.parse(ENV[REDIS_ENV_VAR])
     puts "REDIS URI #{uri.inspect}"
     Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
